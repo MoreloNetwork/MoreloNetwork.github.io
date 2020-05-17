cc787b65018c3d4f3c9182b72972503b0aded67d
mssc89 <oweziz89@gmail.com>
2020-05-18 00:22:14 +0200

    Basic app skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './parts/header/header.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { HomeComponent } from './parts/home/home.component';
+import { FeaturesComponent } from './parts/features/features.component';
+import { DownloadComponent } from './parts/download/download.component';
+import { ContactComponent } from './parts/contact/contact.component';
+import { AboutComponent } from './parts/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FeaturesComponent,
+    DownloadComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
