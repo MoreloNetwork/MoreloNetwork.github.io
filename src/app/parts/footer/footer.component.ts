7af35f33d0c6a9c511f75955b0fb8a414994cf9b
mssc89 <oweziz89@gmail.com>
2020-05-18 10:10:43 +0200

    Implict data type in footer's year, make header code EASIER TO READ

--- src/app/parts/footer/footer.component.ts
+++ src/app/parts/footer/footer.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  year;
+  year: number;
 
-  constructor() { 
+  constructor() {
     this.year = new Date().getUTCFullYear();
   }
 
