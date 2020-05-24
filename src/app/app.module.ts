a1839e27ed076a8d4c92ff1b9157fa20d139b726
mssc89 <oweziz89@gmail.com>
2020-05-24 13:05:41 +0200

    Created custom slider in Home component, closes #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -33,6 +34,7 @@ export function createTranslateLoader(http: HttpClient) {
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     NgbModule,
     TranslateModule.forRoot({
       loader: {
--- src/app/parts/home/home.component.ts
+++ src/app/parts/home/home.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
-declare var $: any;
+import { trigger, transition, animate, style, state } from '@angular/animations';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+      transition('* <=> *', [animate('1s ease-in-out')])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  slide = 1;
 
   constructor() { }
 
   ngOnInit(): void {
-    $('.flexslider').flexslider({
-      animation: "fade",
-      directionNav: false
+
+    // change slide every 5 seconds
+    interval(5000)
+    .subscribe(() => {
+      this.slide++;
+      if (this.slide === 4) {
+        this.slide = 1;
+      }
     });
   }
 
